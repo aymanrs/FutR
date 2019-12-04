27daea71c822000b813cfd838783150064b8188e
xnio94 <xnio94@gmail.com>
2019-12-04 03:39:23 +0100

    hello

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # futr
 
-A new Flutter project.
+remedial teaching is a big market and many people want to make it their job, especially retired teachers and advanced college students, who has the required knowledge and can teaches others. Our mobile App will provide a platform to facilitate this process and help students that are struggling with a subject, by matching them with a teacher. The matching will be based on the subject, teacher’s availability, price, student week points, teacher’s historic performance calculated from the ratings, and other criteria. in details, the app will provide free written/video lessons and on demand personalized video-call lessons, for students seeking help in specific courses. And also, it will be an interactive market for teachers to sell their knowledge and teaching skills.
 
 ## Getting Started
 
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -31,22 +31,21 @@ class LoginScreen extends StatelessWidget {
                 child: Text('Forgot your password ?'),
                 onPressed: () {
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ForgotPasswordScreen()));
+                      context, MaterialPageRoute(builder: (context) => ForgotPasswordScreen()));
                 },
               ),
               FlatButton(
                 child: Text('Don\'t have an account yet ?\nSign up for free!'),
                 onPressed: () {
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => CreateAnAccountScreen()));
+                      context, MaterialPageRoute(builder: (context) => CreateAnAccountScreen()));
                 },
               ),
             ],
-          )
+          ),
+          Image.network(
+            'https://media1.tenor.com/images/b5f479ab1254e2e616023920c1cd7b42/tenor.gif?itemid=13066565',
+          ),
         ],
       ),
     );
